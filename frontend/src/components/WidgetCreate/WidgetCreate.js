a53e65b406343af8dff9bb08aab427e3fa37ca57
Linus Pahl <lpahl@protonmail.com>
2019-01-23 12:32:37 +0100

    FE: Delete not needed prop in WidgetCreate

--- frontend/src/components/WidgetCreate/WidgetCreate.js
+++ frontend/src/components/WidgetCreate/WidgetCreate.js
@@ -52,7 +52,6 @@ class WidgetCreate extends React.Component {
         >
           {createWidget => (
             <WidgetForm
-              createNotificationBanner={createNotificationBanner}
               mode="create"
               mutation={CreateWidget}
               rootPath={rootPath}
