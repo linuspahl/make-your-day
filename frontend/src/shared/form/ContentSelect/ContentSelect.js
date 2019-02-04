6aab78a40c1a6e9f4a8f5d1c0a8b52b6aaa18d55
linuspahl <lpahl@protonmail.com>
2019-02-04 13:39:34 +0100

    FE: Refactor ContentSelect structure and add disabled state

--- frontend/src/shared/form/ContentSelect/ContentSelect.js
+++ frontend/src/shared/form/ContentSelect/ContentSelect.js
@@ -18,7 +18,6 @@ import {
   Options,
   OptionPreview,
   Option,
-  CloseIconWrapper,
 } from './styles'
 
 export default class ContentSelect extends React.Component {
@@ -28,94 +27,33 @@ export default class ContentSelect extends React.Component {
     // isOpen represents if the select dropdown is open
     this.state = { isOpen: false }
 
-    // Toggle select dropdown
-    this.toggleSelect = () => {
-      this.setState({ isOpen: !this.state.isOpen })
-    }
-
-    // Change form state with the selected value
-    this.changeValue = value => {
-      const { onChange, name } = this.props
-      onChange({
-        target: { name, value },
-      })
-    }
-
-    // This function provides the same key navigation like the default html <select>
-    // The only litte difference, the user is able to close the dropdown with the space key
-    // This is not realy a disadvantage for the user and makes the code smaller
-    this.detectKeydown = event => {
-      const { options, value } = this.props
-      const { isOpen } = this.state
-      const { keyCode } = event
-
-      // Disable tab navigation, when select is open and close select instead
-      if (keyCode === 9 && isOpen) {
-        event.preventDefault()
-        this.toggleSelect()
-      }
-
-      // Detect space an enter key and toggle select
-      if (keyCode === 13 || keyCode === 32) {
-        // We need to prevent the default window scrolling for the space key
-        // And the default form submit for the enter key
-        event.preventDefault()
-        this.toggleSelect()
-      }
-
-      // Detect arrow up and arrow down key
-      if (keyCode === 40 || keyCode === 38) {
-        // We need to prevent the default window scrolling for the arrow keys
-        event.preventDefault()
-
-        // Get current option index
-        const currentIndex = options.findIndex(option => option.value === value)
-
-        // On arrow down select next option
-        if (keyCode === 40) {
-          const nextOption = options[currentIndex + 1]
-          if (nextOption) {
-            this.changeValue(nextOption.value)
-          }
-        }
-
-        // On arrow up select previous option
-        if (keyCode === 38) {
-          const prevOption = options[currentIndex - 1]
-          if (prevOption) {
-            this.changeValue(prevOption.value)
-          }
-        }
-      }
-    }
-
-    // onFocus will be triggered, when the user selects / focus the component
-    this.onFocus = () =>
-      document.addEventListener('keydown', this.detectKeydown, false)
-
-    // onBlur will be triggered, when the user deselects / loses the focus of the component
-    this.onBlur = () =>
-      document.removeEventListener('keydown', this.detectKeydown, false)
-
-    // When user clicks on option, update the form ond close the select
-    this.onOptionClick = value => {
-      this.changeValue(value)
-      this.toggleSelect()
-    }
+    this.toggleSelect = this.toggleSelect.bind(this)
+    this.changeValue = this.changeValue.bind(this)
+    this.detectKeydown = this.detectKeydown.bind(this)
+    this.onFocus = this.onFocus.bind(this)
+    this.onBlur = this.onBlur.bind(this)
   }
 
   render() {
-    const { title, value, renderPreview, options, tabIndex, name } = this.props
-    const { isOpen, focusedOption } = this.state
+    const {
+      title,
+      value,
+      renderPreview,
+      options,
+      tabIndex,
+      name,
+      disabled,
+    } = this.props
+    const { isOpen } = this.state
 
     return (
       <Layout>
         <Select
           id={name}
-          tabIndex={tabIndex}
-          onClick={() => this.toggleSelect()}
-          onFocus={this.onFocus}
-          onBlur={this.onBlur}
+          tabIndex={disabled ? 'none' : tabIndex}
+          onClick={() => (!disabled ? this.toggleSelect() : null)}
+          onFocus={disabled ? null : this.onFocus}
+          onBlur={disabled ? null : this.onBlur}
         >
           {value ? value : '-'}
           <Icon title="angle-down" />
@@ -151,5 +89,82 @@ export default class ContentSelect extends React.Component {
         )}
       </Layout>
     )
+  }
+
+  // Toggle select dropdown
+  toggleSelect() {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
+
+  // Change form state with the selected value
+  changeValue(value) {
+    const { onChange, name } = this.props
+    onChange({
+      target: { name, value },
+    })
+  }
+
+  // This function provides the same key navigation like the default html <select>
+  // The only litte difference, the user is able to close the dropdown with the space key
+  // This is not realy a disadvantage for the user and makes the code smaller
+  detectKeydown(event) {
+    const { options, value } = this.props
+    const { isOpen } = this.state
+    const { keyCode } = event
+
+    // Disable tab navigation, when select is open and close select instead
+    if (keyCode === 9 && isOpen) {
+      event.preventDefault()
+      this.toggleSelect()
+    }
+
+    // Detect space an enter key and toggle select
+    if (keyCode === 13 || keyCode === 32) {
+      // We need to prevent the default window scrolling for the space key
+      // And the default form submit for the enter key
+      event.preventDefault()
+      this.toggleSelect()
+    }
+
+    // Detect arrow up and arrow down key
+    if (keyCode === 40 || keyCode === 38) {
+      // We need to prevent the default window scrolling for the arrow keys
+      event.preventDefault()
+
+      // Get current option index
+      const currentIndex = options.findIndex(option => option.value === value)
+
+      // On arrow down select next option
+      if (keyCode === 40) {
+        const nextOption = options[currentIndex + 1]
+        if (nextOption) {
+          this.changeValue(nextOption.value)
+        }
+      }
+
+      // On arrow up select previous option
+      if (keyCode === 38) {
+        const prevOption = options[currentIndex - 1]
+        if (prevOption) {
+          this.changeValue(prevOption.value)
+        }
+      }
+    }
+  }
+
+  // onFocus will be triggered, when the user selects / focus the component
+  onFocus() {
+    document.addEventListener('keydown', this.detectKeydown, false)
+  }
+
+  // onBlur will be triggered, when the user deselects / loses the focus of the component
+  onBlur() {
+    document.removeEventListener('keydown', this.detectKeydown, false)
+  }
+
+  // When user clicks on option, update the form ond close the select
+  onOptionClick(value) {
+    this.changeValue(value)
+    this.toggleSelect()
   }
 }
