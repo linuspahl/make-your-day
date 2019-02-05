d900e86d26fc2d754c4cf157eb8e12f616693bdb
linuspahl <lpahl@protonmail.com>
2019-02-05 02:57:44 +0100

    FE: Adjust CategoryIconOverview spacing

--- frontend/src/components/CategoryIconOverview/styles.js
+++ frontend/src/components/CategoryIconOverview/styles.js
@@ -5,7 +5,7 @@ import ContentBox from 'shared/ContentBox/ContentBox'
 
 export const Layout = styled.div`
   ${props => `
-    padding: 10px;
+    padding: 10px 20px;
 
     border-top: 1px solid ${props.theme.border};
     border-bottom: 1px solid ${props.theme.border};
@@ -13,11 +13,17 @@ export const Layout = styled.div`
     background-color: ${props.theme.contentBoxBg};
     overflow-x: auto;
     overflow-y: hidden;
-    white-space:nowrap
+    white-space: nowrap
   `}
 `
 
 export const IconWrapper = styled.div`
   display: inline-block;
   vertical-align: middle;
+
+  margin-right: 10px;
+
+  &:last-child {
+    margin-right: 0;
+  }
 `
