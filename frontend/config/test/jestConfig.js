a7f70e95352e6738d017621205a14da3fbcf0660
linuspahl <lpahl@protonmail.com>
2019-06-02 15:38:26 +0200

    FE: Enable .test.ts files

--- frontend/config/test/jestConfig.js
+++ frontend/config/test/jestConfig.js
@@ -8,7 +8,7 @@ module.exports = {
   transform: {
     '.(ts|tsx)': 'ts-jest',
   },
-  testRegex: '.*\\.test\\.tsx$',
+  testRegex: '.*\\.test\\.(ts|tsx)$',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/config/test/fileMock',
