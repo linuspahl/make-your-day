7fd9afff6108f0e4253baa4f09b1ecad5a277cde
linuspahl <lpahl@protonmail.com>
2019-06-04 12:05:14 +0200

    FE: Improve Button component test

--- frontend/src/shared/Button/Button.test.tsx
+++ frontend/src/shared/Button/Button.test.tsx
@@ -1,11 +1,75 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
+import colorTheme from 'theme'
+import {
+  cleanup,
+  render,
+  renderWithAppRoot,
+  fireEvent,
+} from '../../../config/test/utils/testUtils'
+import 'jest-styled-components'
 // components
 import Button from './Button'
 
 describe('Button should', (): void => {
-  test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(<Button>Content</Button>)
+  afterEach(cleanup)
+
+  const children = 'My special Button content!'
+  const theme = colorTheme()
+
+  test('display content', (): void => {
+    const { getByText } = render(<Button>{children}</Button>)
+    expect(getByText(children)).toBeInTheDocument()
+  })
+
+  test('work as a link, when target is provided', (): void => {
+    const target = '/example-link'
+    const { getByText } = renderWithAppRoot(
+      <Button to={target}>{children}</Button>
+    )
+    expect(getByText(children).getAttribute('href')).toBe(target)
+    expect(getByText(children).nodeName).toBe('A')
+  })
+
+  test('work as with a click event, when clickAction is provided', (): void => {
+    const onClickEvent = jest.fn()
+    const { getByText } = renderWithAppRoot(
+      <Button clickAction={onClickEvent}>{children}</Button>
+    )
+
+    const leftClick = { button: 0 }
+    fireEvent.click(getByText(children), leftClick)
+
+    expect(onClickEvent).toBeCalledTimes(1)
+  })
+
+  test('use primary colors with context primary', (): void => {
+    const { getByText } = renderWithAppRoot(
+      <Button context="primary">{children}</Button>
+    )
+    expect(getByText(children)).toHaveStyleRule(
+      'background-color',
+      theme.primary
+    )
+  })
+
+  test('use secondary colors with context secondary', (): void => {
+    const { getByText } = renderWithAppRoot(
+      <Button context="secondary">{children}</Button>
+    )
+    expect(getByText(children)).toHaveStyleRule(
+      'background-color',
+      theme.secondary
+    )
+  })
+
+  test('use delete colors with context delete', (): void => {
+    const { getByText } = renderWithAppRoot(
+      <Button context="delete">{children}</Button>
+    )
+    expect(getByText(children)).toHaveStyleRule(
+      'background-color',
+      theme.delete
+    )
   })
 })
