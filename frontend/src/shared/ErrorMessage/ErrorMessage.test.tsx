422539c3ee4a91104e7f42ef05fe99c0db7bd5df
linuspahl <lpahl@protonmail.com>
2019-06-04 16:06:46 +0200

    FE: Improve test for CircleTimer

--- frontend/src/shared/ErrorMessage/ErrorMessage.test.tsx
+++ frontend/src/shared/ErrorMessage/ErrorMessage.test.tsx
@@ -1,13 +1,33 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
+import { render, cleanup } from 'testUtils'
+const utilsMock = jest.genMockFromModule('utils/utils')
 // components
 import ErrorMessage from './ErrorMessage'
 
 describe('ErrorMessage should', (): void => {
-  test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(
-      <ErrorMessage message="Message" error="Error" />
+  const message = 'My special Error message!'
+  const error = 'Apollo error object, or error stack trace'
+  jest.mock(
+    'utils/utils',
+    (): any => {
+      return {
+        logError: jest.fn((p1, p2) => p2),
+      }
+    }
+  )
+
+  afterEach(cleanup)
+
+  test('display content', (): void => {
+    const { getByText } = render(
+      <ErrorMessage message={message} error={error} />
     )
+    expect(getByText(message)).toBeInTheDocument()
+  })
+
+  test('log the provided error', (): void => {
+    render(<ErrorMessage message={message} error={error} />)
+    expect(utilsMock.logError).toBeCalledTimes(1)
   })
 })
