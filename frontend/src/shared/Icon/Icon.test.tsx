70a6a6c10edfb2649b3d35e92be6877d8db566d0
linuspahl <lpahl@protonmail.com>
2019-06-04 16:08:16 +0200

    FE: Improve test for Icon

--- frontend/src/shared/Icon/Icon.test.tsx
+++ frontend/src/shared/Icon/Icon.test.tsx
@@ -1,11 +1,12 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
+import { render } from 'testUtils'
 // components
 import Icon from './Icon'
 
 describe('Icon should', (): void => {
   test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(<Icon title="close" />)
+    const { getByTestId } = render(<Icon title="checkmark" />)
+    expect(getByTestId('Icon')).toBeInTheDocument()
   })
 })
--- frontend/src/shared/Icon/Icon.tsx
+++ frontend/src/shared/Icon/Icon.tsx
@@ -11,7 +11,7 @@ interface Props {
 }
 
 const Icon = (props: Props): JSX.Element => (
-  <i className={`la la-${props.title}`} />
+  <i className={`la la-${props.title}`} data-testid="Icon" />
 )
 
 export default Icon
