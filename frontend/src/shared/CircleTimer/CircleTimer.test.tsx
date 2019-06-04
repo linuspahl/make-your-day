38412007f5cdb16c836810cb85c5bb904fc596be
linuspahl <lpahl@protonmail.com>
2019-06-04 16:09:05 +0200

    FE: Imrpove test for CircleTimer

--- frontend/src/shared/CircleTimer/CircleTimer.test.tsx
+++ frontend/src/shared/CircleTimer/CircleTimer.test.tsx
@@ -1,11 +1,30 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
+import { renderWithAppRoot, cleanup, fireEvent } from 'testUtils'
 // components
 import CircleTimer from './CircleTimer'
 
 describe('CircleTimer should', (): void => {
+  afterEach(cleanup)
+
+  const children = 10
+
   test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(<CircleTimer>Icon</CircleTimer>)
+    const { getByText } = renderWithAppRoot(
+      <CircleTimer>{children}</CircleTimer>
+    )
+    expect(getByText('10')).toBeInTheDocument()
+  })
+
+  test('work with a click event, when clickAction is provided', (): void => {
+    const onClickEvent = jest.fn()
+    const { getByText } = renderWithAppRoot(
+      <CircleTimer clickAction={onClickEvent}>{children}</CircleTimer>
+    )
+
+    const leftClick = { button: 0 }
+    fireEvent.click(getByText(`${children}`), leftClick)
+
+    expect(onClickEvent).toBeCalledTimes(1)
   })
 })
