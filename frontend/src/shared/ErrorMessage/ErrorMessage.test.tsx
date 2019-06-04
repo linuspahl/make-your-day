15d5d1a291f6914f6b0bb60db3f63ccc976ecaed
linuspahl <lpahl@protonmail.com>
2019-06-04 16:10:49 +0200

    FE: Delete not used ErrorMessage test

--- frontend/src/shared/ErrorMessage/ErrorMessage.test.tsx
+++ frontend/src/shared/ErrorMessage/ErrorMessage.test.tsx
@@ -1,22 +1,12 @@
 // libraries
 import * as React from 'react'
 import { render, cleanup } from 'testUtils'
-const utilsMock = jest.genMockFromModule('utils/utils')
 // components
 import ErrorMessage from './ErrorMessage'
 
 describe('ErrorMessage should', (): void => {
   const message = 'My special Error message!'
   const error = 'Apollo error object, or error stack trace'
-  jest.mock(
-    'utils/utils',
-    (): any => {
-      return {
-        logError: jest.fn((p1, p2) => p2),
-      }
-    }
-  )
-
   afterEach(cleanup)
 
   test('display content', (): void => {
@@ -24,10 +14,5 @@ describe('ErrorMessage should', (): void => {
       <ErrorMessage message={message} error={error} />
     )
     expect(getByText(message)).toBeInTheDocument()
-  })
-
-  test('log the provided error', (): void => {
-    render(<ErrorMessage message={message} error={error} />)
-    expect(utilsMock.logError).toBeCalledTimes(1)
   })
 })
