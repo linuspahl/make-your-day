7e074f0a610a68dff99a5f46109827efd1f3046c
linuspahl <lpahl@protonmail.com>
2019-06-06 18:13:36 +0200

    FE: Add userSessionDelete fixtures

--- frontend/src/store/userSession/fixtures.ts
+++ frontend/src/store/userSession/fixtures.ts
@@ -1,5 +1,7 @@
 // interfaces
 import { UserSession } from 'store/userSession/type'
+// graphql
+import { DeleteUserSession } from 'store/userSession/mutation'
 
 export const userSession: UserSession = {
   id: 1,
@@ -7,4 +9,27 @@ export const userSession: UserSession = {
   token: 'tokenxy',
   expiresAt: '1553598046', // unix date
   createdAt: '1553598046',
+}
+
+// Api stubs
+const deleteUserSessionRequest = {
+  request: {
+    query: DeleteUserSession,
+    variables: {
+      id: 1,
+    },
+  },
+}
+export const deleteUserSessionSuccess = {
+  ...deleteUserSessionRequest,
+  result: {
+    data: {
+      deleteUserSession: { id: 1 },
+    },
+  },
+}
+
+export const deleteUserSessionError = {
+  ...deleteUserSessionRequest,
+  error: new Error('deleteUserSession failed'),
 }
