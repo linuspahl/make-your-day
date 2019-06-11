5c1eed21392c5522565df7f6956293e1167999ff
linuspahl <lpahl@protonmail.com>
2019-06-11 17:13:50 +0200

    FE: Change userSession type

--- frontend/src/store/userSession/type.ts
+++ frontend/src/store/userSession/type.ts
@@ -1,7 +1,7 @@
 export interface UserSession {
-  createdAt?: string
+  createdAt?: number
   device?: string
-  expiresAt: string
+  expiresAt: number
   id: number
   token: string
   userId: number
