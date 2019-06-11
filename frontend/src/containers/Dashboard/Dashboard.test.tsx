8588aa600027334a7683652156824405c6bb7623
linuspahl <lpahl@protonmail.com>
2019-06-11 17:21:09 +0200

    FE: Improve Dashboard container test

--- frontend/src/containers/Dashboard/Dashboard.test.tsx
+++ frontend/src/containers/Dashboard/Dashboard.test.tsx
@@ -1,19 +1,41 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
+// utils
+import { renderWithAppRoot, wait } from 'testUtils'
 // components
 import Dashboard from './Dashboard'
 // fixtures
 import { userSession } from 'store/userSession/fixtures'
+import { getWidgetsSuccess, getWidgetsError } from 'store/widget/fixtures'
 
 describe('Dashboard should', (): void => {
-  test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(
+  test('render dashboard and fetch widgets', async (): Promise<void> => {
+    const { getByText } = renderWithAppRoot(
       <Dashboard
         createNotificationBanner={(): void => {}}
-        rootPath="/"
+        rootPath="/dashboard"
         userSession={userSession}
-      />
+      />,
+      { route: '/dashboard', mocks: [getWidgetsSuccess] }
     )
+    await wait()
+    expect(getByText('Notiz')).toBeInTheDocument()
+  })
+
+  test('render dashboard and show error message on widgets fetch error', async (): Promise<
+    void
+  > => {
+    const { getByText } = renderWithAppRoot(
+      <Dashboard
+        createNotificationBanner={(): void => {}}
+        rootPath="/dashboard"
+        userSession={userSession}
+      />,
+      { route: '/dashboard', mocks: [getWidgetsError] }
+    )
+    await wait()
+    expect(
+      getByText('Widgets konnten nicht geladen werden')
+    ).toBeInTheDocument()
   })
 })
