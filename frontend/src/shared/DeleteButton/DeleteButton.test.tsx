edb117862e458318fa97ee5176473883377b4254
linuspahl <lpahl@protonmail.com>
2019-06-11 17:17:14 +0200

    FE: Improve DeleteButton test

--- frontend/src/shared/DeleteButton/DeleteButton.test.tsx
+++ frontend/src/shared/DeleteButton/DeleteButton.test.tsx
@@ -1,22 +1,82 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
+// utils
+import {
+  renderWithAppRoot,
+  fireEvent,
+  leftClickOption,
+  cleanup,
+  wait,
+} from 'testUtils'
 // components
 import DeleteButton from './DeleteButton'
-// store
+// graphql
 import { DeleteUserSession } from 'store/userSession/mutation'
-import { deleteUserSession } from 'store/userSession/update'
+// fixtures
+import { deleteUserSessionSuccess } from 'store/userSession/fixtures'
 
 describe('DeleteButton should', (): void => {
+  const { id: userSessionId } = deleteUserSessionSuccess.request.variables
+  // mock window confirm method
+  afterEach(cleanup)
+
   test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(
+    const onUpdateStub = jest.fn()
+    const onDeleteStub = jest.fn()
+    const { getByText } = renderWithAppRoot(
+      <DeleteButton
+        id={userSessionId}
+        mutation={DeleteUserSession}
+        title="Lösche Sitzing"
+        onUpdate={onUpdateStub}
+        onDelete={onDeleteStub}
+      />,
+      { mocks: [deleteUserSessionSuccess] }
+    )
+    expect(getByText('Löschen')).toBeInTheDocument()
+  })
+
+  test('update store on successful delete', async (): Promise<void> => {
+    window.confirm = (): boolean => true
+
+    const onUpdateStub = jest.fn()
+    const onDeleteStub = jest.fn()
+    const { getByText } = renderWithAppRoot(
       <DeleteButton
         id={1}
-        title={'Titel Eintrag'}
         mutation={DeleteUserSession}
-        onUpdate={deleteUserSession}
-        onDelete={(): void => {}}
-      />
+        onDelete={onDeleteStub}
+        onUpdate={onUpdateStub}
+        title="Lösche Sitzing"
+      />,
+      { mocks: [deleteUserSessionSuccess] }
     )
+    fireEvent.click(getByText('Löschen'), leftClickOption)
+    // Wait for the Mutation component
+    await wait()
+    expect(onUpdateStub).toBeCalledTimes(1)
+  })
+
+  test('not update store, if user does not confirm delete', async (): Promise<
+    void
+  > => {
+    window.confirm = (): boolean => false
+
+    const onUpdateStub = jest.fn()
+    const onDeleteStub = jest.fn()
+    const { getByText } = renderWithAppRoot(
+      <DeleteButton
+        id={1}
+        mutation={DeleteUserSession}
+        title="Lösche Sitzing"
+        onUpdate={onUpdateStub}
+        onDelete={onDeleteStub}
+      />,
+      { mocks: [deleteUserSessionSuccess] }
+    )
+    fireEvent.click(getByText('Löschen'), leftClickOption)
+    // Wait for the Mutation component
+    await wait()
+    expect(onUpdateStub).not.toBeCalled()
   })
 })
