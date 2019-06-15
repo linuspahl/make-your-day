4f73eac77e94fd95700dd2c6e9c130bc68bd272d
linuspahl <lpahl@protonmail.com>
2019-06-15 21:58:34 +0200

    FE: Improve EvaluationOverview test

--- frontend/src/components/EvaluationOverview/EvaluationOverview.test.tsx
+++ frontend/src/components/EvaluationOverview/EvaluationOverview.test.tsx
@@ -1,11 +1,63 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
 // components
 import EvaluationOverview from './EvaluationOverview'
+import { renderWithAppRoot, wait } from 'testUtils'
+// fixtures
+import {
+  evaluation,
+  getEvaluationsError,
+  getEvaluationsSuccess,
+} from 'store/evaluation/fixtures'
 
 describe('EvaluationOverview should', (): void => {
-  test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(<EvaluationOverview rootPath="/" />)
+  test('list fetched evaluations', async (): Promise<void> => {
+    const { getByText } = renderWithAppRoot(
+      <EvaluationOverview rootPath="/evaluations" />,
+      { mocks: [getEvaluationsSuccess] }
+    )
+    // Wait for the Query component
+    await wait()
+    expect(getByText(evaluation.title)).toBeInTheDocument()
+  })
+
+  test('show info, when evaluation fetching fails', async (): Promise<void> => {
+    const { getByText } = renderWithAppRoot(
+      <EvaluationOverview rootPath="/evaluations" />,
+      { mocks: [getEvaluationsError] }
+    )
+    // Wait for the Query component
+    await wait()
+
+    expect(
+      getByText('Auswertungen konnten nicht geladen werden')
+    ).toBeInTheDocument()
+  })
+
+  test('show loading spinner, while fetching evaluations', (): void => {
+    const { getByTestId } = renderWithAppRoot(
+      <EvaluationOverview rootPath="/evaluations" />
+    )
+    // without `await wait()` the Query component will be in the loading state
+    expect(getByTestId('CenteredSpinner')).toBeInTheDocument()
+  })
+
+  test('show info, when there are no existing evaluations', async (): Promise<
+    void
+  > => {
+    const { getByText } = renderWithAppRoot(
+      <EvaluationOverview rootPath="/evaluations" />,
+      {
+        mocks: [
+          {
+            ...getEvaluationsSuccess,
+            result: { data: { getEvaluations: [] } },
+          },
+        ],
+      }
+    )
+    await wait()
+
+    expect(getByText('Kein Eintrag vorhanden')).toBeInTheDocument()
   })
 })
