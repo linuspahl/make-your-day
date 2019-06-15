b1b10e309b2ab7e89cbb6500e837c289aa8d65c7
linuspahl <lpahl@protonmail.com>
2019-06-15 21:56:31 +0200

    FE: Improve SubcategoryForm test

--- frontend/src/components/SubcategoryForm/SubcategoryForm.test.tsx
+++ frontend/src/components/SubcategoryForm/SubcategoryForm.test.tsx
@@ -1,19 +1,82 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
-// components
-import SubcategoryForm from './SubcategoryForm'
+// utils
+import {
+  cleanup,
+  fireEvent,
+  leftClickOption,
+  Matcher,
+  renderWithAppRoot,
+  SelectorMatcherOptions,
+  wait,
+} from 'testUtils'
 // fixtures
-import { category } from 'store/category/fixtures'
+import { subcategoryCreate, category } from 'store/category/fixtures'
+// components
+import SubcategoryForm from 'components/SubcategoryForm/SubcategoryForm'
+
+const componentProps = {
+  rootPath: `/category/${category.id}/subcategory/create`,
+}
+
+export const initSubcategoryForm = async (
+  getByLabelText: (
+    text: Matcher,
+    options?: SelectorMatcherOptions
+  ) => HTMLElement
+): Promise<void> => {
+  // fill form
+  fireEvent.change(getByLabelText('Name'), {
+    target: { value: category.subcategories[0].title },
+  })
+  await wait()
+}
 
 describe('SubcategoryForm should', (): void => {
-  test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(
+  afterEach(cleanup)
+
+  test('submit with correct values', (): void => {
+    const submitActionStub = jest.fn()
+    const { getByText, getByLabelText } = renderWithAppRoot(
       <SubcategoryForm
-        rootPath="/"
+        {...componentProps}
+        mode="create"
+        submitAction={submitActionStub}
         parentCategory={category}
-        submitAction={(): void => {}}
       />
     )
+    initSubcategoryForm(getByLabelText)
+    // Submit form
+    fireEvent.click(getByText('Erstellen'), leftClickOption)
+    expect(submitActionStub).toBeCalledTimes(1)
+    expect(submitActionStub).toBeCalledWith(subcategoryCreate)
+  })
+
+  test('submit correct with initial values', (): void => {
+    const submitActionStub = jest.fn()
+    const { getByText } = renderWithAppRoot(
+      <SubcategoryForm
+        {...componentProps}
+        initialData={subcategoryCreate}
+        mode="create"
+        submitAction={submitActionStub}
+        parentCategory={category}
+      />
+    )
+    // Submit form
+    fireEvent.click(getByText('Erstellen'), leftClickOption)
+    expect(submitActionStub).toBeCalledTimes(1)
+    expect(submitActionStub).toBeCalledWith(subcategoryCreate)
+  })
+
+  test('use edit button as default submit button', (): void => {
+    const { getByText } = renderWithAppRoot(
+      <SubcategoryForm
+        {...componentProps}
+        submitAction={(): void => {}}
+        parentCategory={category}
+      />
+    )
+    expect(getByText('Bearbeiten')).toBeInTheDocument()
   })
 })
