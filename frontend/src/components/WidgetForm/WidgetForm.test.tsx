0e5c31f33ecb0a8ba1038129d69ba01df716aaf0
linuspahl <lpahl@protonmail.com>
2019-06-15 21:57:51 +0200

    FE: Improve WidgetForm test

--- frontend/src/components/WidgetForm/WidgetForm.test.tsx
+++ frontend/src/components/WidgetForm/WidgetForm.test.tsx
@@ -1,13 +1,104 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
+// utils
+import {
+  cleanup,
+  fireEvent,
+  leftClickOption,
+  Matcher,
+  renderWithAppRoot,
+  SelectorMatcherOptions,
+  wait,
+} from 'testUtils'
+import { widgetPositionOptions, widgetTypeOptions } from 'params'
 // components
 import WidgetForm from './WidgetForm'
+// fixtures
+import { widgetCreate } from 'store/widget/fixtures'
+import { evaluation } from 'store/evaluation/fixtures'
+
+const componentProps = {
+  rootPath: '/widget/create',
+}
+
+export const initWidgetForm = async (
+  getByLabelText: (
+    text: Matcher,
+    options?: SelectorMatcherOptions
+  ) => HTMLElement,
+  getByText: (text: Matcher, options?: SelectorMatcherOptions) => HTMLElement
+): Promise<void> => {
+  // fill form
+  fireEvent.change(getByLabelText('Name'), {
+    target: { value: widgetCreate.title },
+  })
+  fireEvent.mouseDown(getByLabelText('Art'), leftClickOption)
+  fireEvent.click(getByText(widgetTypeOptions[0].title), leftClickOption)
+
+  fireEvent.mouseDown(getByLabelText('Position'), leftClickOption)
+  fireEvent.click(getByText(widgetPositionOptions[0].title), leftClickOption)
+
+  await wait()
+}
 
 describe('WidgetForm should', (): void => {
-  test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(
-      <WidgetForm rootPath="/" submitAction={(): void => {}} />
+  afterEach(cleanup)
+
+  test('submit with correct values', (): void => {
+    const submitActionStub = jest.fn()
+    const { getByText, getByLabelText } = renderWithAppRoot(
+      <WidgetForm
+        {...componentProps}
+        mode="create"
+        submitAction={submitActionStub}
+      />
     )
+    initWidgetForm(getByLabelText, getByText)
+    // Submit form
+    fireEvent.click(getByText('Erstellen'), leftClickOption)
+    expect(submitActionStub).toBeCalledTimes(1)
+    expect(submitActionStub).toBeCalledWith(widgetCreate)
+  })
+
+  test('submit correct with initial values', (): void => {
+    const submitActionStub = jest.fn()
+    const { getByText } = renderWithAppRoot(
+      <WidgetForm
+        {...componentProps}
+        mode="create"
+        initialData={widgetCreate}
+        submitAction={submitActionStub}
+      />
+    )
+    // Submit form
+    fireEvent.click(getByText('Erstellen'), leftClickOption)
+    expect(submitActionStub).toBeCalledTimes(1)
+    expect(submitActionStub).toBeCalledWith(widgetCreate)
+  })
+
+  test('list evaluations in widget select', async (): Promise<void> => {
+    const { getByText, getByLabelText } = renderWithAppRoot(
+      <WidgetForm
+        {...componentProps}
+        mode="create"
+        submitAction={(): void => {}}
+        evaluations={[evaluation]}
+      />
+    )
+    fireEvent.mouseDown(getByLabelText('Art'), leftClickOption)
+    fireEvent.click(getByText('Auswertung'), leftClickOption)
+
+    fireEvent.mouseDown(
+      getByLabelText('Verknüpfte Auswertung'),
+      leftClickOption
+    )
+    expect(getByText(evaluation.title)).toBeInTheDocument()
+  })
+
+  test('use edit button as default submit button', (): void => {
+    const { getByText } = renderWithAppRoot(
+      <WidgetForm {...componentProps} submitAction={(): void => {}} />
+    )
+    expect(getByText('Bearbeiten')).toBeInTheDocument()
   })
 })
