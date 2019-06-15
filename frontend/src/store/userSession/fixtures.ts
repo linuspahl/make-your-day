1b2f86b413a51c0169d9216ea7d732f4e97a5a8f
linuspahl <lpahl@protonmail.com>
2019-06-15 21:17:24 +0200

    FE: Adjust userSession fixture

--- frontend/src/store/userSession/fixtures.ts
+++ frontend/src/store/userSession/fixtures.ts
@@ -1,19 +1,70 @@
 // interfaces
 import { UserSession } from 'store/userSession/type'
 // graphql
+import { LoginUser } from 'store/userSession/mutation'
 import { DeleteUserSession } from 'store/userSession/mutation'
 import { GetUserSessions } from 'store/userSession/query'
 
 export const userSession: UserSession = {
   id: 1,
   userId: 1,
-  token: 'tokenxy',
+  token: 'TOKEN1',
   device: 'Mac',
+  role: 'Admin',
   expiresAt: 1560248192861, // unix date with ms
   createdAt: 1560248190861,
 }
 
+export const userSession2: UserSession = {
+  ...userSession,
+  id: 2,
+  token: 'TOKEN2',
+  expiresAt: 1560248192845, // unix date with ms
+  createdAt: 1560248190845,
+}
+
 // Api stubs
+const loginUserRequest = {
+  request: {
+    query: LoginUser,
+    variables: {
+      username: 'Username 1',
+      password: 'Password 1',
+      device: 'Mac',
+    },
+  },
+}
+export const loginUserSuccess = {
+  ...loginUserRequest,
+  result: {
+    data: {
+      loginUser: {
+        id: userSession.id,
+        token: userSession.userId,
+        role: 'Admin',
+        userSettings: [
+          {
+            value: true,
+            setting: {
+              type: 'nightmode',
+            },
+          },
+        ],
+        userSession: {
+          id: userSession.id,
+          token: userSession.token,
+          expiresAt: userSession.expiresAt,
+          createdAt: userSession.createdAt,
+        },
+      },
+    },
+  },
+}
+
+export const loginUserError = {
+  ...loginUserRequest,
+  error: new Error('loginUser failed'),
+}
 
 const getUserSessionsRequest = {
   request: {
@@ -24,9 +75,14 @@ export const getUserSessionsSuccess = {
   ...getUserSessionsRequest,
   result: {
     data: {
-      getUserSessions: [userSession],
+      getUserSessions: [userSession, userSession2],
     },
   },
+}
+
+export const getUserSessionsError = {
+  ...getUserSessionsRequest,
+  error: new Error('getUserSessions failed'),
 }
 
 const deleteUserSessionRequest = {
