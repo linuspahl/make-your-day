6c3b282ceb0a0765baca906c4d76b9f1a4a7c4fb
linuspahl <lpahl@protonmail.com>
2019-06-17 12:20:06 +0200

    FE: Refactor CategoryOverview Query state handling

--- frontend/src/components/CategoryOverview/CategoryOverview.test.tsx
+++ frontend/src/components/CategoryOverview/CategoryOverview.test.tsx
@@ -4,11 +4,7 @@ import * as React from 'react'
 import CategoryOverview from './CategoryOverview'
 import { renderWithAppRoot, wait } from 'testUtils'
 // fixtures
-import {
-  category,
-  getCategoriesError,
-  getCategoriesSuccess,
-} from 'store/category/fixtures'
+import { category, getCategoriesSuccess } from 'store/category/fixtures'
 
 describe('CategoryOverview should', (): void => {
   test('list fetched categories', async (): Promise<void> => {
@@ -19,42 +15,5 @@ describe('CategoryOverview should', (): void => {
     // Wait for the Query component
     await wait()
     expect(getByText(category.title)).toBeInTheDocument()
-  })
-
-  test('show info, when category fetching fails', async (): Promise<void> => {
-    const { getByText } = renderWithAppRoot(
-      <CategoryOverview rootPath="/categories" />,
-      { mocks: [getCategoriesError] }
-    )
-    // Wait for the Query component
-    await wait()
-
-    expect(
-      getByText('Kategorien konnten nicht geladen werden')
-    ).toBeInTheDocument()
-  })
-
-  test('show loading spinner, while fetching categories', (): void => {
-    const { getByTestId } = renderWithAppRoot(
-      <CategoryOverview rootPath="/categories" />
-    )
-    // without `await wait()` the Query component will be in the loading state
-    expect(getByTestId('CenteredSpinner')).toBeInTheDocument()
-  })
-
-  test('show info, when there are no existing categories', async (): Promise<
-    void
-  > => {
-    const { getByText } = renderWithAppRoot(
-      <CategoryOverview rootPath="/categories" />,
-      {
-        mocks: [
-          { ...getCategoriesSuccess, result: { data: { getCategories: [] } } },
-        ],
-      }
-    )
-    await wait()
-
-    expect(getByText('Kein Eintrag vorhanden')).toBeInTheDocument()
   })
 })
--- frontend/src/components/CategoryOverview/CategoryOverview.tsx
+++ frontend/src/components/CategoryOverview/CategoryOverview.tsx
@@ -1,19 +1,15 @@
 // libraries
 import * as React from 'react'
 import styled from 'styled-components'
-import { Query } from 'react-apollo'
-import { ApolloError } from 'apollo-boost'
 // components
 import ActionIcon from 'shared/list/ActionIcon/ActionIcon'
 import ActionRow from 'shared/form/ActionRow/ActionRow'
 import Button from 'shared/Button/Button'
-import CenteredSpinner from 'shared/CenteredSpinner/CenteredSpinner'
 import DeleteIcon from 'shared/list/DeleteIcon/DeleteIcon'
-import ErrorMessage from 'shared/ErrorMessage/ErrorMessage'
 import FadeTransition from 'shared/FadeTransition/FadeTransition'
 import H1 from 'shared/H1/H1'
 import ListItem from 'shared/list/ListItem/ListItem'
-import NoResult from 'shared/NoResult/NoResult'
+import QueryStateHandler from 'shared/QueryStateHandler/QueryStateHandler'
 // graphql
 import { GetCategories } from 'store/category/query'
 import { DeleteCategory } from 'store/category/mutation'
@@ -36,62 +32,27 @@ const CategoryOverview = (props: Props): JSX.Element => {
     <FadeTransition fullWidth>
       <H1 context="page">Kategorien verwalten</H1>
 
-      <Query query={GetCategories}>
-        {({
-          loading,
-          error,
-          data,
-        }: {
-          loading: boolean
-          data: { getCategories: CategoryPlain[] }
-          error?: ApolloError
-        }): JSX.Element[] | JSX.Element => {
-          if (loading) return <CenteredSpinner />
-          if (error) {
-            return (
-              <ErrorMessage
-                error={error}
-                message="Kategorien konnten nicht geladen werden"
-              />
-            )
-          }
-          if (data.getCategories.length === 0) return <NoResult />
+      <QueryStateHandler
+        errorMessage="Andere Sitzungen konnten nicht geladen werden"
+        query={GetCategories}
+        queryName="getCategories"
+      >
+        {(categories: CategoryPlain[]): JSX.Element => {
           return (
             <List>
-              {data.getCategories.map(
+              {categories.map(
                 (category: CategoryPlain): JSX.Element => (
-                  <ListItem key={category.id} spaceBetween>
-                    {category.title}
-                    <div>
-                      {category.hasSubcategories && (
-                        <ActionIcon
-                          to={`${rootPath}/${category.id}/subcategories`}
-                          icon="list-ul"
-                          ariaLabel={`Kategorie ${
-                            category.title
-                          } Subkategorien bearbeiten`}
-                        />
-                      )}
-                      <ActionIcon
-                        to={`${rootPath}/edit/${category.id}`}
-                        icon="edit"
-                        ariaLabel={`Kategorie ${category.title} bearbeiten`}
-                      />
-                      <DeleteIcon
-                        ariaLabel={`Kategorie ${category.title} löschen`}
-                        id={category.id}
-                        mutation={DeleteCategory}
-                        onUpdate={deleteCategory}
-                        title={category.title}
-                      />
-                    </div>
-                  </ListItem>
+                  <CategoryListItem
+                    category={category}
+                    key={category.id}
+                    rootPath={rootPath}
+                  />
                 )
               )}
             </List>
           )
         }}
-      </Query>
+      </QueryStateHandler>
 
       <ActionRow>
         <Button context="primary" to={`${rootPath}/create`}>
@@ -102,4 +63,39 @@ const CategoryOverview = (props: Props): JSX.Element => {
   )
 }
 
+const CategoryListItem = (props: {
+  category: CategoryPlain
+  rootPath: string
+}): JSX.Element => {
+  const {
+    category: { id, title, hasSubcategories },
+    rootPath,
+  } = props
+  return (
+    <ListItem spaceBetween>
+      {title}
+      <div>
+        {hasSubcategories && (
+          <ActionIcon
+            to={`${rootPath}/${id}/subcategories`}
+            icon="list-ul"
+            ariaLabel={`Kategorie ${title} Subkategorien bearbeiten`}
+          />
+        )}
+        <ActionIcon
+          to={`${rootPath}/edit/${id}`}
+          icon="edit"
+          ariaLabel={`Kategorie ${title} bearbeiten`}
+        />
+        <DeleteIcon
+          ariaLabel={`Kategorie ${title} löschen`}
+          id={id}
+          mutation={DeleteCategory}
+          onUpdate={deleteCategory}
+          title={title}
+        />
+      </div>
+    </ListItem>
+  )
+}
 export default CategoryOverview
