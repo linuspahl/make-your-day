be1b1330076a7a9d2443bbfbdef7924517756752
linuspahl <lpahl@protonmail.com>
2019-06-18 23:56:20 +0200

    FE: Improve SubcategoryOverview test

--- frontend/src/components/SubcategoryOverview/SubcategoryOverview.test.tsx
+++ frontend/src/components/SubcategoryOverview/SubcategoryOverview.test.tsx
@@ -1,13 +1,25 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
+// utils
+import { renderWithAppRoot, wait } from 'testUtils'
 // components
 import SubcategoryOverview from './SubcategoryOverview'
+// fixtrues
+import {
+  subcategory,
+  category,
+  getCategoryPlainWithChildrenSuccess,
+} from 'store/category/fixtures'
 
 describe('SubcategoryOverview should', (): void => {
-  test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(
-      <SubcategoryOverview rootPath="/" />
+  test('display content', async (): Promise<void> => {
+    const { getByText } = renderWithAppRoot(
+      <SubcategoryOverview
+        rootPath={`category/${category.id}/subcategories`}
+      />,
+      { mocks: [getCategoryPlainWithChildrenSuccess] }
     )
+    await wait()
+    expect(getByText(subcategory.title)).toBeInTheDocument()
   })
 })
