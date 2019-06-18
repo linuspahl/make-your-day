ed85748e63c2a8fd4afcb90c7083d9e8c539a5ed
linuspahl <lpahl@protonmail.com>
2019-06-18 23:56:45 +0200

    FE: Improve EvaluationWidget test

--- frontend/src/components/EvaluationWidget/EvlaluationWidget.test.tsx
+++ frontend/src/components/EvaluationWidget/EvlaluationWidget.test.tsx
@@ -1,15 +1,21 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
+// utils
+import { renderWithAppRoot, wait, mockWindow } from 'testUtils'
 // components
 import EvaluationWidget from './EvaluationWidget'
 // fixtures
 import { evaluation } from 'store/evaluation/fixtures'
 
 describe('EvaluationWidget should', (): void => {
-  test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(
-      <EvaluationWidget evaluation={evaluation} />
+  mockWindow()
+
+  test('render evaluation', async (): Promise<void> => {
+    const { container } = renderWithAppRoot(
+      <EvaluationWidget evaluation={{ ...evaluation, type: 'linechart' }} />
     )
+    // Wait for chart library to finish render
+    await wait()
+    expect(container.getElementsByClassName('ct-chart-line')).toHaveLength(1)
   })
 })
