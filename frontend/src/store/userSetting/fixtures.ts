cdcffc44042bdbdc97b3220ea755ecabcfbe5cfc
linuspahl <lpahl@protonmail.com>
2019-06-18 23:41:29 +0200

    FE: Add getUserSetting and deleteUserSetting stub fixture

--- frontend/src/store/userSetting/fixtures.ts
+++ frontend/src/store/userSetting/fixtures.ts
@@ -1,10 +1,57 @@
 // interfaces
 import { UserSetting } from 'store/userSetting/type'
+// graphql
+import {
+  CreateUserSetting,
+  DeleteUserSetting,
+} from 'store/userSetting/mutation'
 // fixtures
 import { setting } from 'store/setting/fixtures'
 
 export const userSetting: UserSetting = {
   id: 1,
-  value: 'false',
+  value: 'true',
   setting,
+}
+
+// # Api stubs
+
+// ## createUserSetting
+const createUserSettingRequest = {
+  request: {
+    query: CreateUserSetting,
+    variables: { settingId: setting.id },
+  },
+}
+export const createUserSettingSuccess = {
+  ...createUserSettingRequest,
+  result: {
+    data: {
+      createUserSetting: userSetting,
+    },
+  },
+}
+export const createUserSettingError = {
+  ...createUserSettingRequest,
+  error: new Error('createUserSetting failed'),
+}
+
+// ## deleteUserSetting
+const deleteUserSettingRequest = {
+  request: {
+    query: DeleteUserSetting,
+    variables: { settingId: setting.id },
+  },
+}
+export const deleteUserSettingSuccess = {
+  ...deleteUserSettingRequest,
+  result: {
+    data: {
+      deleteUserSetting: true,
+    },
+  },
+}
+export const deleteUserSettingError = {
+  ...deleteUserSettingRequest,
+  error: new Error('deleteUserSetting failed'),
 }
