aca29297c4582ec80d5da0639f1ba3350a3f7e59
linuspahl <lpahl@protonmail.com>
2019-06-18 23:48:49 +0200

    FE: Improve CategoryCreate test

--- frontend/src/components/CategoryCreate/CategoryCreate.test.tsx
+++ frontend/src/components/CategoryCreate/CategoryCreate.test.tsx
@@ -1,13 +1,68 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
+// utils
+import initCategoryForm from 'components/CategoryForm/__tests__/initCategoryForm'
+import {
+  renderWithAppRoot,
+  fireEvent,
+  leftClickOption,
+  wait,
+  cleanup,
+} from 'testUtils'
 // components
 import CategoryCreate from './CategoryCreate'
+// fixtures
+import {
+  createCategorySuccess,
+  createCategoryError,
+  category,
+} from 'store/category/fixtures'
 
 describe('CategoryCreate should', (): void => {
-  test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(
-      <CategoryCreate rootPath="/" createNotificationBanner={(): void => {}} />
+  const propsFixture = {
+    rootPath: '/category/create',
+  }
+  afterEach(cleanup)
+
+  test('show notification banner on successful create ', async (): Promise<
+    void
+  > => {
+    const createNotificationBannerStub = jest.fn()
+    const { getByLabelText, getByText } = renderWithAppRoot(
+      <CategoryCreate
+        {...propsFixture}
+        createNotificationBanner={createNotificationBannerStub}
+      />,
+      { mocks: [createCategorySuccess] }
     )
+    initCategoryForm(getByLabelText, getByText)
+    fireEvent.click(getByText('Erstellen'), leftClickOption)
+    await wait()
+    expect(createNotificationBannerStub).toBeCalledTimes(1)
+    expect(createNotificationBannerStub).toBeCalledWith({
+      message: `Kategorie ${category.title} erfolgreich erstellt`,
+      type: 'success',
+    })
+  })
+
+  test('show notification banner on unsuccessful create', async (): Promise<
+    void
+  > => {
+    const createNotificationBannerStub = jest.fn()
+    const { getByLabelText, getByText } = renderWithAppRoot(
+      <CategoryCreate
+        {...propsFixture}
+        createNotificationBanner={createNotificationBannerStub}
+      />,
+      { mocks: [createCategoryError] }
+    )
+    initCategoryForm(getByLabelText, getByText)
+    fireEvent.click(getByText('Erstellen'), leftClickOption)
+    await wait()
+    expect(createNotificationBannerStub).toBeCalledTimes(1)
+    expect(createNotificationBannerStub).toBeCalledWith({
+      message: 'Erstellung der Kategorie fehlgeschlagen',
+      type: 'error',
+    })
   })
 })
