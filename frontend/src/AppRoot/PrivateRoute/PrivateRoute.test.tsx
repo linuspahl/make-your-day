d8f4c3ea2ecfcba4d0549436cf998644fc565c2e
linuspahl <lpahl@protonmail.com>
2019-07-02 15:14:50 +0200

    FE: Improve PrivateRoute test

--- frontend/src/AppRoot/PrivateRoute/PrivateRoute.test.tsx
+++ frontend/src/AppRoot/PrivateRoute/PrivateRoute.test.tsx
@@ -1,21 +1,45 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
+import { renderWithAppRoot, cleanup } from 'testUtils'
 // components
 import PrivateRoute from './PrivateRoute'
 // fixtures
 import { userSession } from 'store/userSession/fixtures'
+import { Switch } from 'react-router'
 
 describe('PrivateRoute should', (): void => {
-  test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(
-      <PrivateRoute
-        clearLocalStorage={(): void => {}}
-        component={(): JSX.Element => <div>Content</div>}
-        createNotificationBanner={(): void => {}}
-        path="/"
-        userSession={userSession}
-      />
+  const children = 'Content'
+  afterEach(cleanup)
+
+  test('render content', (): void => {
+    const { getByText } = renderWithAppRoot(
+      <Switch>
+        <PrivateRoute
+          clearLocalStorage={(): void => {}}
+          component={(): JSX.Element => <div>{children}</div>}
+          createNotificationBanner={(): void => {}}
+          path="/dashboard"
+          userSession={userSession}
+        />
+      </Switch>,
+      { route: '/dashboard' }
     )
+    expect(getByText(children)).toBeInTheDocument()
+  })
+
+  test('not render content, when no userSession is provided', (): void => {
+    const { queryByText } = renderWithAppRoot(
+      <Switch>
+        <PrivateRoute
+          clearLocalStorage={(): void => {}}
+          component={(): JSX.Element => <div>{children}</div>}
+          createNotificationBanner={(): void => {}}
+          path="/dashboard"
+          userSession={null}
+        />
+      </Switch>,
+      { route: '/dashboard' }
+    )
+    expect(queryByText(children)).not.toBeInTheDocument()
   })
 })
