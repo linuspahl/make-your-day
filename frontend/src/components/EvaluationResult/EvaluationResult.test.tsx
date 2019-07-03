abe5d969c0248c4248f6281f47d2eccdaa639600
linuspahl <lpahl@protonmail.com>
2019-07-03 13:06:08 +0200

    FE: Improve EvaluationResult test

--- frontend/src/components/EvaluationResult/EvaluationResult.test.tsx
+++ frontend/src/components/EvaluationResult/EvaluationResult.test.tsx
@@ -1,22 +1,29 @@
 // libraries
-import * as React from 'react'
+import React from 'react'
 // utils
-import { renderWithAppRoot, wait } from 'testUtils'
+import { renderWithAppRoot, wait, mockWindow } from 'testUtils'
 // components
 import EvaluationResult from './EvaluationResult'
 // fixtures
 import { getEvaluationSuccess, evaluation } from 'store/evaluation/fixtures'
 
 describe('EvaluationResult should', (): void => {
+  mockWindow()
+
   test('render without crashing', async (): Promise<void> => {
     const { container } = renderWithAppRoot(
-      <EvaluationResult rootPath={`/evaluations/${evaluation.id}`} />,
+      <EvaluationResult rootPath={`/evaluations`} />,
       {
         mocks: [getEvaluationSuccess],
+        mockWrappingRoute: true,
+        route: `/evaluations/view/${evaluation.id}`,
+        routePath: '/evaluations/view/:id',
       }
     )
+    // wait for getEvaluation
     await wait()
-    // TODO: FIX MOCK
+    // wait for chart to render
+    await wait()
     expect(container.getElementsByClassName('ct-chart-line')).toHaveLength(1)
   })
 })
