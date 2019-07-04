5745574f7c120812b7c996d96468d40e5dc6d5d6
linuspahl <lpahl@protonmail.com>
2019-07-04 12:58:29 +0200

    FE: Add delay for loading spinner, needed for dynamic imports

--- frontend/src/shared/CenteredSpinner/CenteredSpinner.tsx
+++ frontend/src/shared/CenteredSpinner/CenteredSpinner.tsx
@@ -3,13 +3,30 @@ import * as React from 'react'
 // components
 import { Layout, Spinner, DoubleBouncer1, DoubleBouncer2 } from './styles'
 
-const CenteredSpinner = (): JSX.Element => (
-  <Layout data-testid="CenteredSpinner">
-    <Spinner>
-      <DoubleBouncer1 />
-      <DoubleBouncer2 />
-    </Spinner>
-  </Layout>
-)
+interface Props {
+  pastDelay?: boolean
+  error?: string
+  retry?: () => void
+}
+
+const CenteredSpinner = (props: Props): JSX.Element => {
+  if (props.error) {
+    return (
+      <div>
+        Error! <button onClick={props.retry}>Retry</button>
+      </div>
+    )
+  } else if (props.pastDelay) {
+    return (
+      <Layout data-testid="CenteredSpinner">
+        <Spinner>
+          <DoubleBouncer1 />
+          <DoubleBouncer2 />
+        </Spinner>
+      </Layout>
+    )
+  }
+  return null
+}
 
 export default CenteredSpinner
