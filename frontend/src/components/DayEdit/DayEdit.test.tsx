776f877fe2aab3e69d9cb465efbce3a23ca63247
linuspahl <lpahl@protonmail.com>
2019-07-04 12:35:25 +0200

    FE: Change DayEdit test

--- frontend/src/components/DayEdit/DayEdit.test.tsx
+++ frontend/src/components/DayEdit/DayEdit.test.tsx
@@ -1,11 +1,29 @@
 // libraries
 import * as React from 'react'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
+// utils
+import { renderWithAppRoot, wait } from 'testUtils'
 // components
 import DayEdit from './DayEdit'
 
+import { getRecordsSuccess } from 'store/record/fixtures'
+
 describe('DayEdit should', (): void => {
-  test('render without crashing', (): void => {
-    ShallowRenderer.createRenderer().render(<DayEdit />)
+  test('should list records', async (): Promise<void> => {
+    const { getByText } = renderWithAppRoot(<DayEdit />, {
+      mocks: [
+        {
+          ...getRecordsSuccess,
+          request: {
+            ...getRecordsSuccess.request,
+            variables: { date: '2010-10-10' },
+          },
+        },
+      ],
+      route: `/timeline/2010-10-10`,
+      routePath: `/timeline/:date`,
+      mockWrappingRoute: true,
+    })
+    // wait for getRecords
+    await wait()
   })
 })
