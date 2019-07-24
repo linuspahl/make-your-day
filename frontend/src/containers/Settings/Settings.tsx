f92ae625c2ac65738d258baa2adb638cf564a02b
linuspahl <lpahl@protonmail.com>
2019-07-24 15:00:34 +0200

    FE: Remove ContentBox component from Settings container

--- frontend/src/containers/Settings/Settings.tsx
+++ frontend/src/containers/Settings/Settings.tsx
@@ -2,7 +2,6 @@
 import * as React from 'react'
 import { Route } from 'react-router-dom'
 // components
-import ContentBox from 'shared/ContentBox/ContentBox'
 import PageLayout from 'components/PageLayout/PageLayout'
 import UserSettingsOverview from 'components/UserSettingsOverview/UserSettingsOverview'
 import UserSessionsOverview from 'components/UserSessionsOverview/UserSessionsOverview'
@@ -23,33 +22,31 @@ interface Props {
 const Settings = (props: Props): JSX.Element => {
   return (
     <PageLayout userSession={props.userSession} rootPath={props.rootPath}>
-      <ContentBox role="main">
-        <Route
-          exact
-          path={props.rootPath}
-          render={(): JSX.Element => (
-            <UserSettingsOverview
-              clearLocalStorage={props.clearLocalStorage}
-              createNotificationBanner={props.createNotificationBanner}
-              rootPath={props.rootPath}
-              updateLocalStorage={props.updateLocalStorage}
-              userSession={props.userSession}
-              userSettings={props.userSettings}
-            />
-          )}
-        />
-        <Route
-          exact
-          path={`${props.rootPath}/sessions`}
-          render={(): JSX.Element => (
-            <UserSessionsOverview
-              clearLocalStorage={props.clearLocalStorage}
-              createNotificationBanner={props.createNotificationBanner}
-              currentUserSession={props.userSession}
-            />
-          )}
-        />
-      </ContentBox>
+      <Route
+        exact
+        path={props.rootPath}
+        render={(): JSX.Element => (
+          <UserSettingsOverview
+            clearLocalStorage={props.clearLocalStorage}
+            createNotificationBanner={props.createNotificationBanner}
+            rootPath={props.rootPath}
+            updateLocalStorage={props.updateLocalStorage}
+            userSession={props.userSession}
+            userSettings={props.userSettings}
+          />
+        )}
+      />
+      <Route
+        exact
+        path={`${props.rootPath}/sessions`}
+        render={(): JSX.Element => (
+          <UserSessionsOverview
+            clearLocalStorage={props.clearLocalStorage}
+            createNotificationBanner={props.createNotificationBanner}
+            currentUserSession={props.userSession}
+          />
+        )}
+      />
     </PageLayout>
   )
 }
