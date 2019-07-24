5fbc8c2dd2909dfae56b4a590bbff5de6725c2c2
linuspahl <lpahl@protonmail.com>
2019-07-24 14:55:47 +0200

    FE: Implement PageQueryHandler for UserSessionsOverview

--- frontend/src/components/UserSessionsOverview/UserSessionsOverview.tsx
+++ frontend/src/components/UserSessionsOverview/UserSessionsOverview.tsx
@@ -6,12 +6,11 @@ import dayjs from 'dayjs'
 import DeleteIcon from 'shared/list/DeleteIcon/DeleteIcon'
 import LogoutIcon from 'shared/list/LogoutIcon/LogoutIcon'
 import H1 from 'shared/H1/H1'
-import FadeTransition from 'shared/FadeTransition/FadeTransition'
 import Grid from 'shared/grid/Grid/Grid'
 import GridHead from 'shared/grid/GridHead/GridHead'
 import GridBody from 'shared/grid/GridBody/GridBody'
 import GridCell from 'shared/grid/GridCell/GridCell'
-import QueryStateHandler from 'shared/QueryStateHandler/QueryStateHandler'
+import PageQueryHandler from 'shared/PageQueryHandler/PageQueryHandler'
 // qraphql
 import { GetUserSessions } from 'store/userSession/query'
 import { DeleteUserSession } from 'store/userSession/mutation'
@@ -26,45 +25,60 @@ interface Props {
   currentUserSession: UserSession
 }
 
+interface PageQueryResult {
+  data: { getUserSessions: UserSession[] }
+  status: { getUserSessions: JSX.Element }
+}
+
 const UserSessionOverview = (props: Props): JSX.Element => (
-  <FadeTransition>
-    <H1 context="page">Angemeldete Geräte</H1>
-    <QueryStateHandler
-      errorMessage="Andere Sitzungen konnten nicht geladen werden"
-      query={GetUserSessions}
-      queryName="getUserSessions"
-    >
-      {(userSessions: UserSession[]): JSX.Element => {
-        return (
-          <Grid>
-            <GridHead>
-              <GridCell>Gerät</GridCell>
-              <GridCell>Gültig bis</GridCell>
-              <GridCell />
-            </GridHead>
-            <GridBody columnAmount={3}>
-              {userSessions.map(
-                (userSession: UserSession): JSX.Element => {
-                  return (
-                    <ListItem
-                      clearLocalStorage={props.clearLocalStorage}
-                      createNotificationBanner={props.createNotificationBanner}
-                      isCurrentSession={
-                        userSession.expiresAt ===
-                        props.currentUserSession.expiresAt
-                      }
-                      key={userSession.id}
-                      userSession={userSession}
-                    />
-                  )
-                }
-              )}
-            </GridBody>
-          </Grid>
-        )
-      }}
-    </QueryStateHandler>
-  </FadeTransition>
+  <PageQueryHandler
+    errorMessages={{
+      getUserSessions: 'Andere Sitzungen konnten nicht geladen werden',
+    }}
+    query={GetUserSessions}
+    queryNames={['getUserSessions']}
+  >
+    {({
+      data: { getUserSessions: userSessions },
+      status: { getUserSessions: userSessionsQueryStatus },
+    }: PageQueryResult): JSX.Element => {
+      return (
+        <React.Fragment>
+          <H1 context="page">Angemeldete Geräte</H1>
+          {userSessionsQueryStatus}
+          {!userSessionsQueryStatus && userSessions && (
+            <Grid>
+              <GridHead>
+                <GridCell>Gerät</GridCell>
+                <GridCell>Gültig bis</GridCell>
+                <GridCell />
+              </GridHead>
+              <GridBody columnAmount={3}>
+                {userSessions.map(
+                  (userSession: UserSession): JSX.Element => {
+                    return (
+                      <ListItem
+                        clearLocalStorage={props.clearLocalStorage}
+                        createNotificationBanner={
+                          props.createNotificationBanner
+                        }
+                        isCurrentSession={
+                          userSession.expiresAt ===
+                          props.currentUserSession.expiresAt
+                        }
+                        key={userSession.id}
+                        userSession={userSession}
+                      />
+                    )
+                  }
+                )}
+              </GridBody>
+            </Grid>
+          )}
+        </React.Fragment>
+      )
+    }}
+  </PageQueryHandler>
 )
 
 const ListItem = (props: {
