5404a24c01e11c200f7b2d59f43542177499ec1c
linuspahl <lpahl@protonmail.com>
2019-07-31 02:40:34 +0200

    FE: Add different font family for H2

--- frontend/src/shared/H2/styles.tsx
+++ frontend/src/shared/H2/styles.tsx
@@ -6,7 +6,9 @@ import TextBig from 'shared/text/TextBig/TextBig'
 export const Element = styled(TextBig)`
   margin: 0;
 
+  font-family: 'Titillium Web', Arial, Helvetica, sans-serif;
   font-size: ${(props): string => `${props.theme.fontSizes.mobile.big}rem`};
+  font-weight: 300;
   hyphens: auto;
   text-overflow: ellipsis;
   overflow: hidden;
