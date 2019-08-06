3276764adb627daec314a81081b12cd2ff74a035
linuspahl <lpahl@protonmail.com>
2019-08-06 21:26:34 +0200

    FE: Only show hover and active styling for CategoryIcon, if the component is clickable

--- frontend/src/shared/CategoryIcon/styles.tsx
+++ frontend/src/shared/CategoryIcon/styles.tsx
@@ -2,10 +2,10 @@
 import styled from 'styled-components'
 
 interface CircleProps {
-  size?: number
-  color?: string
-  to?: string
   as?: React.ReactNode
+  color?: string
+  size?: number
+  to?: string
 }
 
 export const Circle = styled.div<CircleProps>`
@@ -25,18 +25,24 @@ export const Circle = styled.div<CircleProps>`
   box-shadow: 0 0.1875rem 0.0625rem -0.125rem rgba(0, 0, 0, 0.2),
     0 0.125rem 0.125rem 0 rgba(0, 0, 0, 0.14),
     0 0.0625rem 0.3125rem 0 rgba(0, 0, 0, 0.12);
-  &:hover {
-    box-shadow: 0 0.125rem 0.25rem -0.0625rem rgba(0, 0, 0, 0.2),
-      0 0.25rem 0.3125rem 0 rgba(0, 0, 0, 0.14),
-      0 0.0625rem 0.625rem 0 rgba(0, 0, 0, 0.12);
-  }
-  &:active {
-    box-shadow: 0 0.3125rem 0.3125rem -0.1875rem rgba(0, 0, 0, 0.2),
-      0 0.5rem 0.625rem 0.0625rem rgba(0, 0, 0, 0.14),
-      0 0.1875rem 0.875rem 0.125rem rgba(0, 0, 0, 0.12);
-  }
 
-  &:visited {
-    color: ${(props): string => props.theme.categoryText[props.color]};
-  }
+  /* If the prop is a link, we want to make the styling it more interactive */
+  ${(props): string =>
+    props.to &&
+    `
+      &:hover {
+        box-shadow: 0 0.125rem 0.25rem -0.0625rem rgba(0, 0, 0, 0.2),
+          0 0.25rem 0.3125rem 0 rgba(0, 0, 0, 0.14),
+          0 0.0625rem 0.625rem 0 rgba(0, 0, 0, 0.12);
+      }
+      &:active {
+        box-shadow: 0 0.3125rem 0.3125rem -0.1875rem rgba(0, 0, 0, 0.2),
+          0 0.5rem 0.625rem 0.0625rem rgba(0, 0, 0, 0.14),
+          0 0.1875rem 0.875rem 0.125rem rgba(0, 0, 0, 0.12);
+      }
+
+      &:visited {
+        color: ${props.theme.categoryText[props.color]};
+      } 
+  `}
 `
