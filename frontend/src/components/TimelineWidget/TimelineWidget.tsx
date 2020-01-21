7f67033216c7fd2295296fbd0b3b62e80d40b314
Linus Pahl <lpahl@protonmail.com>
2020-01-21 23:57:46 +0100

    FE: Implement hooks for DashboardWidget

--- frontend/src/components/TimelineWidget/TimelineWidget.tsx
+++ frontend/src/components/TimelineWidget/TimelineWidget.tsx
@@ -1,5 +1,5 @@
 // libraries
-import React from 'react'
+import React, { useMemo } from 'react'
 import dayjs from 'dayjs'
 import { sortBy, fill } from 'lodash'
 // components
@@ -33,101 +33,93 @@ const LoadingPlaceholder = (
   </PlaceholderGroup>
 )
 
-export default class TimelineWidget extends React.Component {
-  public constructor(props: {}) {
-    super(props)
-    this.prepareTimeline = this.prepareTimeline.bind(this)
+const getRecordAmount = (record: Record, category: Category): number => {
+  if (category.hasUnit && category.type !== 'counter') {
+    return record.amount
   }
-
-  public render(): JSX.Element {
-    // We want to fetch all posts of the last seven days.
-    const createdAt = new Date()
-    createdAt.setDate(createdAt.getDate() - 7)
-
-    return (
-      <Outer>
-        <Layout>
-          <QueryStateHandler
-            errorMessage="Einträge konnten nicht geladen werden"
-            queryName="getRecords"
-            query={GetRecords}
-            loadingPlaceholder={LoadingPlaceholder}
-          >
-            {(records: Record[]): JSX.Element => {
-              const timeline = this.prepareTimeline(records)
-
-              return (
-                <React.Fragment>
-                  {sortBy(Object.values(timeline), 'date').map(
-                    (day: DayEntry): JSX.Element => (
-                      <TimelineWidgetDay
-                        categories={Object.values(day.categories)}
-                        date={day.date}
-                        key={day.date}
-                      />
-                    )
-                  )}
-                </React.Fragment>
-              )
-            }}
-          </QueryStateHandler>
-        </Layout>
-      </Outer>
-    )
-  }
-
-  private prepareTimeline(records: Record[]): { [key: string]: DayEntry } {
-    // The timeline object has an array of records as input
-    // for each day with a record, we'll create an entry
-    // Based on the day we will group all related records by category
-
-    const timeline: { [key: string]: DayEntry } = {}
-
-    // First we start by adding a key to the timeline for the last seven days
-    for (let index = 0; index <= 6; index++) {
-      // Define a new date and set the date based on the index
-      // E.g. today - 0 results in days, today - 1 will result in yesterdays date, etc.
-      const day = dayjs().subtract(index, 'day')
-      const dayDate = dayjs(day).format('YYYY-MM-DD')
-      timeline[dayDate] = {
-        categories: {},
-        date: dayDate,
-      }
-    }
-
-    records.forEach((record): void => {
-      const { category: recordCategory } = record
-      const category = recordCategory.parent || recordCategory
-      const categoryKey = `${category.id}`
-      // get createdAt date and format to string
-      const recordCreatedAtUnix = parseInt(record.createdAt)
-      const createdAtDay = dayjs(recordCreatedAtUnix).format('YYYY-MM-DD')
-      if (timeline[createdAtDay]) {
-        // Check if an entry for the category exists
-        let categoryEnry = timeline[createdAtDay].categories[categoryKey]
-        if (!categoryEnry) {
-          // If no entry exists, create one and set the amount of the record
-          categoryEnry = {
-            ...category,
-            recordAmountSum: this.getRecordAmount(record, category),
-          }
-        } else {
-          // Otherwise only sum up the record amount
-          categoryEnry.recordAmountSum =
-            categoryEnry.recordAmountSum +
-            this.getRecordAmount(record, category)
-        }
-        timeline[createdAtDay].categories[categoryKey] = categoryEnry
-      }
-    })
-
-    return timeline
-  }
-
-  private getRecordAmount(record: Record, category: Category): number {
-    if (category.hasUnit && category.type !== 'counter') {
-      return record.amount
-    }
-    return 1
-  }
+  return 1
 }
+
+const prepareTimeline = (records: Record[]): { [key: string]: DayEntry } => {
+  // The timeline object has an array of records as input
+  // for each day with a record, we'll create an entry
+  // Based on the day we will group all related records by category
+
+  const timeline: { [key: string]: DayEntry } = {}
+
+  // First we start by adding a key to the timeline for the last seven days
+  for (let index = 0; index <= 6; index++) {
+    // Define a new date and set the date based on the index
+    // E.g. today - 0 results in days, today - 1 will result in yesterdays date, etc.
+    const day = dayjs().subtract(index, 'day')
+    const dayDate = dayjs(day).format('YYYY-MM-DD')
+    timeline[dayDate] = {
+      categories: {},
+      date: dayDate,
+    }
+  }
+
+  records.forEach((record): void => {
+    const { category: recordCategory } = record
+    const category = recordCategory.parent || recordCategory
+    const categoryKey = `${category.id}`
+    // get createdAt date and format to string
+    const recordCreatedAtUnix = parseInt(record.createdAt)
+    const createdAtDay = dayjs(recordCreatedAtUnix).format('YYYY-MM-DD')
+    if (timeline[createdAtDay]) {
+      // Check if an entry for the category exists
+      let categoryEnry = timeline[createdAtDay].categories[categoryKey]
+      if (!categoryEnry) {
+        // If no entry exists, create one and set the amount of the record
+        categoryEnry = {
+          ...category,
+          recordAmountSum: getRecordAmount(record, category),
+        }
+      } else {
+        // Otherwise only sum up the record amount
+        categoryEnry.recordAmountSum =
+          categoryEnry.recordAmountSum + getRecordAmount(record, category)
+      }
+      timeline[createdAtDay].categories[categoryKey] = categoryEnry
+    }
+  })
+
+  return timeline
+}
+
+const TimelineWidget = (): JSX.Element => {
+  // We want to fetch all posts of the last seven days.
+  const createdAt = new Date()
+  createdAt.setDate(createdAt.getDate() - 7)
+  return (
+    <Outer>
+      <Layout>
+        <QueryStateHandler
+          errorMessage="Einträge konnten nicht geladen werden"
+          queryName="getRecords"
+          query={GetRecords}
+          loadingPlaceholder={LoadingPlaceholder}
+        >
+          {(records: Record[]): JSX.Element => {
+            const timeline = prepareTimeline(records)
+            return (
+              <React.Fragment>
+                {sortBy(Object.values(timeline), 'date').map(
+                  (day: DayEntry): JSX.Element => (
+                    <TimelineWidgetDay
+                      categories={Object.values(day.categories)}
+                      date={day.date}
+                      key={day.date}
+                    />
+                  )
+                )}
+              </React.Fragment>
+            )
+          }}
+        </QueryStateHandler>
+      </Layout>
+    </Outer>
+  )
+}
+
+export default TimelineWidget
