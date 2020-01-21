446401c7657e3928119e6602d513ec2c6198d3a3
Linus Pahl <lpahl@protonmail.com>
2020-01-21 23:54:32 +0100

    FE: Implement hooks for InAppLayout

--- frontend/src/components/InAppLayout/InAppLayout.tsx
+++ frontend/src/components/InAppLayout/InAppLayout.tsx
@@ -1,5 +1,5 @@
 // libraries
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { withRouter, RouteComponentProps } from 'react-router-dom'
 // utils
 import { getRootPath } from 'utils/utils'
@@ -21,94 +21,75 @@ interface State {
   navigationState: NavigationState
 }
 
-class InAppLayout extends React.Component<Props, State> {
-  public constructor(props: Props) {
-    super(props)
+const InAppLayout = (props: Props): JSX.Element => {
+  const [navigationOpen, setNavigationOpen] = useState(false)
+  const [animateNavOnClose, setAnimateNavOnClose] = useState(false)
 
-    this.state = {
-      navigationState: { open: false, animateOnClose: true },
-    }
-
-    this.toggleNavigation = this.toggleNavigation.bind(this)
-    this.detectTouchstart = this.detectTouchstart.bind(this)
-    this.detectKeydown = this.detectKeydown.bind(this)
+  const toggleNavigation = (animateOnClose = true): void => {
+    setAnimateNavOnClose(animateOnClose)
+    setNavigationOpen(!navigationOpen)
   }
 
-  public componentDidMount(): void {
-    document.addEventListener('touchstart', this.detectTouchstart)
-    document.addEventListener('keydown', this.detectKeydown)
-  }
-
-  public componentWillUnmount(): void {
-    document.removeEventListener('touchstart', this.detectTouchstart)
-    document.removeEventListener('keydown', this.detectKeydown)
-  }
-
-  public componentWillReceiveProps(nextProps: Props): void {
-    // If the route changes and the navigation is open, we want to close it
-    if (this.state.navigationState.open) {
-      if (nextProps.location.pathname !== this.props.location.pathname) {
-        this.toggleNavigation(false)
-      } else {
-        this.toggleNavigation()
-      }
-    }
-  }
-
-  public render(): JSX.Element {
-    const {
-      userSession,
-      children,
-      location: { pathname },
-    } = this.props
-    const { navigationState } = this.state
-    const rootPath = getRootPath(pathname)
-    return (
-      <Layout>
-        <ChildrenWrapper>{children}</ChildrenWrapper>
-
-        <BottomNavigation
-          isNavVisible={navigationState.open}
-          toggleNavigation={this.toggleNavigation}
-        >
-          <CategoryIconOverview context="horizontal-scroll" />
-        </BottomNavigation>
-
-        {userSession && userSession.token && (
-          <Navigation
-            rootPath={rootPath}
-            state={navigationState}
-            toggleAction={this.toggleNavigation}
-          />
-        )}
-      </Layout>
-    )
-  }
-
-  private toggleNavigation(animateOnClose = true): void {
-    this.setState({
-      navigationState: {
-        animateOnClose,
-        open: !this.state.navigationState.open,
-      },
-    })
-  }
-
-  private detectKeydown(event: KeyboardEvent): void {
-    // Detect when:
-    // - or when the user click the alt key (desktop)
-    if (event.keyCode === 18) {
-      this.toggleNavigation()
-    }
-  }
-
-  private detectTouchstart(event: TouchEvent): void {
+  const detectTouchstart = (event: TouchEvent): void => {
     // Detect when:
     // - user clicks with two fingers (mobile)
     if (event.type && event.touches.length === 2) {
-      this.toggleNavigation()
+      toggleNavigation()
     }
   }
+
+  const detectKeydown = (event: KeyboardEvent): void => {
+    // Detect when:
+    // - or when the user click the alt key (desktop)
+    if (event.keyCode === 18) {
+      toggleNavigation()
+    }
+  }
+
+  useEffect((): (() => void) => {
+    document.addEventListener('touchstart', detectTouchstart)
+    document.addEventListener('keydown', detectKeydown)
+
+    return (): void => {
+      document.removeEventListener('touchstart', detectTouchstart)
+      document.removeEventListener('keydown', detectKeydown)
+    }
+  }, [])
+
+  // If the route changes and the navigation is open, we want to close it
+  useEffect((): void => {
+    if (navigationOpen) {
+      setNavigationOpen(true)
+    }
+  }, [location.pathname])
+
+  const {
+    userSession,
+    children,
+    location: { pathname },
+  } = props
+  const rootPath = getRootPath(pathname)
+  return (
+    <Layout>
+      <ChildrenWrapper>{children}</ChildrenWrapper>
+
+      <BottomNavigation
+        isNavVisible={navigationOpen}
+        toggleNavigation={toggleNavigation}
+      >
+        <CategoryIconOverview context="horizontal-scroll" />
+      </BottomNavigation>
+
+      {userSession && userSession.token && (
+        <Navigation
+          rootPath={rootPath}
+          open={navigationOpen}
+          animateOnClose={animateNavOnClose}
+          toggleAction={toggleNavigation}
+        />
+      )}
+    </Layout>
+  )
 }
 
 export default withRouter(InAppLayout)
